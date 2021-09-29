021da6a0db4b2950fc29bdc53f6803af3eb96dd0
andres142 <andresmfonseca@outlook.com>
2021-09-29 13:12:26 -0500

    Pagina master

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function mostrar() {
+    document.getElementById("sidebar").style.width = "200px";
+    document.getElementById("abrir-menu").style.marginLeft = "200px";
+    document.getElementById("contenido").style.marginLeft = "200px";
+    document.getElementById("abrir").style.display = "none";
+    document.getElementById("cerrar").style.display = "inline";
+}
+
+function ocultar() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("abrir-menu").style.marginLeft = "0";
+    document.getElementById("contenido").style.marginLeft = "0px";
+    document.getElementById("abrir").style.display = "inline";
+    document.getElementById("cerrar").style.display = "none";
+}
