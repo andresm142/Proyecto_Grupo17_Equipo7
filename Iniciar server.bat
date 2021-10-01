6178222baecbe01ea340daf952ae8d5e2b4bd597
andres142 <andresmfonseca@outlook.com>
2021-09-30 21:02:05 -0500

    Plantillas

---
+++ Iniciar server.bat
@@ -0,0 +1,8 @@
+@echo off
+
+echo PARA ABRIR LA PAGINA COLOCAR EN EL NAVEGADOR LA DIRECCION [31mhttp://localhost:[0m 
+echo AL FINAL DE LOS [31m:[0m COLOCOAR EL PUERTO QUE SE MUESTRA AQUI.
+echo .
+echo .
+
+python -m http.server
