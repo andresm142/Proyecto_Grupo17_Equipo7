bebf65ce0704ef457f35525dd91e84f07e9b8390
andres142 <andresmfonseca@outlook.com>
2021-10-05 21:10:17 -0500

    Actualizacion

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.wrappers import response
 
 app=Flask(__name__)
+
 @app.route('/')
-def wtv():
+def login():
     return render_template('Login.html')
+
+@app.route('/master', methods=['POST'])
+def master():
+    print(request.form)
+    usuario= request.form['email']
+    clave=request.form['clave']
+    print(usuario+' '+clave)
+    return redirect(url_for('MasterPage.html'))
+    # return render_template('MasterPage.html')
+
+app.run(debug = True)
