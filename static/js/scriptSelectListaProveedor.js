1d4a58aad14ec0a388d221a12235b44829023e56
Jenniffer <rosanegra2323@gmail.com>
2021-10-12 21:42:01 -0500

    Correccion editar listas

---
+++ static/js/scriptSelectListaProveedor.js
@@ -0,0 +1,15 @@
+// DESPLIEGE DE LA OPCION DE LA CALIFICACION
+const selectedCalificacion = document.querySelector(".selected-calificacion");
+const optionsContainerCalificacion = document.querySelector(".options-container-calificacion");
+const optionsListCalificacion = document.querySelectorAll(".option-calificacion");
+
+selectedCalificacion.addEventListener("click", () => {
+    optionsContainerCalificacion.classList.toggle("active");
+});
+
+optionsListCalificacion.forEach(o => {
+    o.addEventListener("click", () => {
+        selectedCalificacion.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainerCalificacion.classList.remove("active");
+    });
+});
